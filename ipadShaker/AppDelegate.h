981011114180965ab68c586d7d6f2d9960f31d57
Orsi Juhasz <orsi@Orsi-Juhaszs-MacBook-Pro.local>
2011-09-06 17:35:17 +0200

    Initial Commit

---
+++ ipadShaker/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ipadShaker
+//
+//  Created by Orsolya Juhasz on 9/6/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ipadShaker/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ipadShaker
+//
+//  Created by Orsolya Juhasz on 9/6/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
